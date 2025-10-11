b5200c64e68b0603701d7ddf95d2601f5d0316f0
rohit <rohit73304@gmail.com>
2025-10-11 11:00:50 +0530

    Initial Commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { navLinks } from '../constants'
+
+const NavBar = () => {
+    return (
+        <header>
+            <nav className='flex items-center justify-center gap-10'>
+                <img src="/logo.svg" alt="Apple Logo" />
+
+                <ul className=''>
+                    {navLinks.map(({ label }) => (
+                        <li key={label}>
+                            <a href={label}>{label}</a>
+                        </li>
+                    ))}
+                </ul>
+
+                <div className='flex-center gap-6'>
+                    <button>
+                        <img src="/search.svg" alt="Search" />
+                    </button>
+
+
+                    <button>
+                        <img src="/cart.svg" alt="Cart" />
+                    </button>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
---
+++ src/constants/index.js
@@ -0,0 +1,12 @@
+export const navLinks = [
+                        { label: 'Store' }
+                        , { label: 'Mac' }
+                        , { label: 'iPad' }
+                        , { label: 'iPhone' }
+                        , { label: 'Watch' }
+                        , { label: 'AirPods' }
+                        , { label: 'TV & Home' }
+                        , { label: 'Only on Apple' }
+                        , { label: 'Accessories' }
+                        , { label: 'Support' }
+                    ]
